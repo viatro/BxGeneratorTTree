6b1697875a683be0128a20e186d8507f8412a8eb
viatro <vi-at@ya.ru>
2016-01-11 03:15:41 +0100

    suppress grep output

--- build_g4bx2_ttree.sh
+++ build_g4bx2_ttree.sh
@@ -6,15 +6,15 @@ g4bx2_dir=${offline_dir}/bxmc/g4bx2
 rsync -hhruP --exclude '.git' --exclude '.gitignore' --exclude 'README.md' ${BXUSERS}/viatro/BxGeneratorTTree/ ${g4bx2_dir}/
 
 cd ${g4bx2_dir}/src
-if ! grep "BxGeneratorTTree" BxPrimaryGeneratorActionMessenger.cc; then
-	sed -i '0,/#include /s//#include "BxGeneratorTTree.hh"\n&/' ./BxPrimaryGeneratorActionMessenger.cc 
-	sed -i '/fSelectCmd->SetCandidates/ i\candidates += " TTree";' ./BxPrimaryGeneratorActionMessenger.cc
-	sed -i '0,/(newValue ==/s// (newValue == "TTree") {\n\t\tBxOutputVertex::Get()->SetGenerator(50);\n\t\tfGeneratorPrimary->SetBxGenerator(new BxGeneratorTTree);\n\t} else if &/' ./BxPrimaryGeneratorActionMessenger.cc
+if ! grep -q "BxGeneratorTTree" BxPrimaryGeneratorActionMessenger.cc; then
+	sed -i '0,/#include /s//#include "BxGeneratorTTree.hh"\n&/' BxPrimaryGeneratorActionMessenger.cc 
+	sed -i '/fSelectCmd->SetCandidates/ i\candidates += " TTree";' BxPrimaryGeneratorActionMessenger.cc
+	sed -i '0,/(newValue ==/s// (newValue == "TTree") {\n\t\tBxOutputVertex::Get()->SetGenerator(50);\n\t\tfGeneratorPrimary->SetBxGenerator(new BxGeneratorTTree);\n\t} else if &/' BxPrimaryGeneratorActionMessenger.cc
 fi
 
 cd ${g4bx2_dir}
-grep "TreePlayer" ./CMakeLists.txt || sed -i "s/\${ROOT_LIBRARIES}/& TreePlayer/g" ./CMakeLists.txt
-grep "ttree.mac" ./CMakeLists.txt || sed -i '/gun.mac/ i\ttree.mac' ./CMakeLists.txt
+grep -q "TreePlayer" CMakeLists.txt || sed -i "s/\${ROOT_LIBRARIES}/& TreePlayer/g" CMakeLists.txt
+grep -q "ttree.mac" CMakeLists.txt || sed -i '/gun.mac/ i\ttree.mac' CMakeLists.txt
 source please_source_me
 
 cd ${g4bx2_dir}/build
