2ed004a1442049c39b7cef346bf905d88d677088
viatro <vi-at@ya.ru>
2016-04-13 03:06:25 +0200

    change modes selecting to allow them work together

--- src/BxStackingTTreeMessenger.cc
+++ src/BxStackingTTreeMessenger.cc
@@ -13,6 +13,7 @@
 #include "G4UIdirectory.hh"
 #include "G4UIcmdWithAString.hh"
 #include "G4UIcmdWithADoubleAndUnit.hh"
+#include "G4AnalysisUtilities.hh"
 
 BxStackingTTreeMessenger::BxStackingTTreeMessenger(BxStackingTTree* stack) : fStacking(stack) {
     fDirectory = new G4UIdirectory("/bx/stack/ttree/");
@@ -37,9 +38,18 @@ BxStackingTTreeMessenger::~BxStackingTTreeMessenger() {
 void BxStackingTTreeMessenger::SetNewValue(G4UIcommand* cmd, G4String newValue) {
     if (cmd == fModeCmd) {
         newValue.toLower();
-        if (newValue == "neutron")  fStacking->SetMode(0, true);
-        else if (newValue == "ra_decay")   fStacking->SetMode(1, true);
-        else if (newValue == "muon")   fStacking->SetMode(2, true);
+        std::vector<G4String> parameters;
+        G4Analysis::Tokenize(newValue, parameters);
+        
+        if (parameters[0] == "all") fStacking->SetMode(true);
+        else if (parameters[0] == "none" || parameters[0] == "") fStacking->SetMode(false);
+        else {
+            for (size_t i = 0; i < parameters.size(); ++i) {
+                if (parameters[i] == "neutron") fStacking->SetMode(0, true);
+                else if (parameters[i] == "ra_decay") fStacking->SetMode(1, true);
+                else if (parameters[i] == "muon") fStacking->SetMode(2, true);
+            }
+        }
         BxLog(routine) << "TTree Stacking \"Mode\" UI-cmd is \"" << newValue << "\"" << endlog;
     } else if (cmd == fTimeCutCmd) {
         fStacking->SetTimeCut(fTimeCutCmd->ConvertToDimensionedDouble(newValue));
